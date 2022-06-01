e1638f4f0ec06f4b64cfa349b361273cedb02455
araujo44 <106673968+araujo44@users.noreply.github.com>
2022-06-01 12:16:24 -0300

    Update matrix.h
    Indentação do struct Matrix

--- matrix.h
+++ matrix.h
@@ -2,12 +2,12 @@
 #define MATRIX_H
 
 typedef struct {
-int *data;
-int n_rows;
-int n_cols;
-int stride_rows;
-int stride_cols;
-int offset;
+  int *data;
+  int n_rows;
+  int n_cols;
+  int stride_rows;
+  int stride_cols;
+  int offset;
 } Matrix;
 
 Matrix create_matrix(int *data,int n_rows, int n_cols);
