4ee9c5902b69b7485211a5bc082c2d8590db39ba
Alexandremw76 <104449595+Alexandremw76@users.noreply.github.com>
2022-06-07 15:58:49 -0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -25,9 +25,15 @@ void print_matrix(Matrix matrix);
 /* funções para manipulação de dimensões */
 
 /* funções de agregação */
-
+int min(Matrix matrix);
+int max(Matrix matrix);
+int argmin(Matrix matrix);
+int argmax(Matrix matrix);
 /* funções de operações aritméticas */
-
+Matrix add(Matrix matrix_1, Matrix matrix_2); 
+Matrix sub(Matrix matrix_1, Matrix matrix_2); 
+Matrix division(Matrix matrix_1, Matrix matrix_2); 
+Matrix mul(Matrix matrix_1, Matrix matrix_2);  
 
 #endif
 
